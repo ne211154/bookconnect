0e2398608710a49f6891074894f14c09393c0755
koto8 <tokoya0416@gmail.com>
2023-09-06 23:11:27 +0900

    9/6 23:11 マージファイルを途中まで作成

--- bookconnect/db/migrate/20230830134340_create_users.rb
+++ bookconnect/db/migrate/20230830134340_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :users, primary_key: "UserNo." do |t|
       t.integer :UserNo., null: false
       t.string :ID, null: false
       t.string :Name, null: false
-      t.sting :Email, null: false
+      t.string :Email, null: false
       t.string :Password, null: false
       t.boolern :AccessX, null: false
       t.boolern :AccessAmazon, null: false
--- bookconnect/db/migrate/20230830134444_create_bookshelves.rb
+++ bookconnect/db/migrate/20230830134444_create_bookshelves.rb
@@ -1,7 +1,15 @@
 class CreateBookshelves < ActiveRecord::Migration[7.0]
   def change
-    create_table :bookshelves do |t|
-
+    create_table :bookshelves, primary_key: "BookshelfNo." do |t|
+      t.integer :BookshlfNo., null: false
+      t.integer :UserNo., null: false
+      t.string :Comment, null: false
+      t.integer :FrameNo., null: false
+      t.integer :SizedecorationNo., null: false
+      t.integer :NosizedecorationNo., null: false
+      t.integer :Like-count, null: false
+      t.integer :Bookshelf-rank, null: false
+      t.integer :Preview-image, null: false
       t.timestamps
     end
   end
