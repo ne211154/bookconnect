0381bf18caf081700fbc9d1721313587e45c4f89
koto8 <tokoya0416@gmail.com>
2023-09-27 12:01:08 +0900

    migrationファイルの作成

--- bookconnect/db/migrate/20230830134612_create_possesionbooks.rb
+++ bookconnect/db/migrate/20230830134612_create_possesionbooks.rb
@@ -1,7 +1,9 @@
 class CreatePossesionbooks < ActiveRecord::Migration[7.0]
   def change
-    create_table :possesionbooks do |t|
-
+    create_table :possesionbooks, primary_key: "PossessionbookNo." do |t|
+      t.integer :PossessionbookNo., null: false
+      t.string :Link, null: false
+      t.string :Thoughts, null: false
       t.timestamps
     end
   end
