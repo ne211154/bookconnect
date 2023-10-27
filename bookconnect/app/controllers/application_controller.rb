8aa7dc38af4b23c593e976f051a7eaa430d7f8e0
ne211156 <130714664+ne211156@users.noreply.github.com>
2023-10-27 09:22:08 +0900

    仮

--- bookconnect/app/controllers/application_controller.rb
+++ bookconnect/app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    private def current_member
+        User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_member
 end
